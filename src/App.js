06167aa9191f2f5919a02820b46dcf32ecfe86dd
Елизавета <elisava2018@mail.ru>
2023-12-07 21:33:05 +0300

    123

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Каргина Елизавета Олеговна";
+  const greeting = "Это тестовый проект Каргина Елизавета Олеговна!";
 
   return (
     <div className="App">
